92f0876399fd152b67ab83b68a860f3dfbcf5244
akshay5568 <adityajangid124@gmail.com>
2026-06-26 14:57:07 +0530

    Today I tried to solve  CombinationItretor (Backtracking) LeetCode problem.

--- Recursion/Patterns/Purmetation.java
+++ Recursion/Patterns/Purmetation.java
@@ -5,22 +5,37 @@ import java.util.List;
 
 public class Purmetation {
     public static void main(String[] args) {
-            String str = "cat";
-            purmeutation(new int[]{1,2,3},new ArrayList<>(),new boolean[]{false,false,false});
+            String str = "a1b2";
+//            purmeutation(new int[]{1,1,2},new ArrayList<>(),new boolean[]{false,false,false});
+          boolean[] used = new boolean[str.length()];
+            StringPurmutation("",str);
     }
+
 
     static void purmeutation(int [] arr, List<Integer> temp, boolean [] used) {
             if (temp.size() >= arr.length) {
-                System.out.println(temp);
+//                System.out.println(temp);
                 return;
             }
             for (int i=0; i<arr.length; i++){
                 if (used[i]) continue;
                 temp.add(arr[i]);
+                System.out.println("I : "+i + " " + temp);
                 used[i] = true;
                 purmeutation(arr,temp,used);
                 temp.removeLast();
+                System.out.println("I : "+i + " " + temp);
                 used[i] = false;
             }
+    }
+
+    static void StringPurmutation(String p,String up){
+        if (up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        for (int i=0; i<up.length(); i++){
+            StringPurmutation(p+up.charAt(i),up.substring(0,i) + up.substring(i+1,up.length()));
+        }
     }
 }

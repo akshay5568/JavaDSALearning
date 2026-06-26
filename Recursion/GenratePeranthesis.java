92d34b52f77f857656c1a8f01a7b499d41dbe478
akshay5568 <adityajangid124@gmail.com>
2026-06-26 14:57:07 +0530

    Today I tried to solve GenratePeranthesis (Backtraking) LeetCode problem.

--- Recursion/GenratePeranthesis.java
+++ Recursion/GenratePeranthesis.java
@@ -3,9 +3,9 @@ package Recursion;
 import java.util.List;
 
 public class GenratePeranthesis {
-    public static void main(String[] args) {
-
-    }
+//    public static void main(String[] args) {
+//
+//    }
 
 //    public static List<String> generateParenthesis(int n) {
 //

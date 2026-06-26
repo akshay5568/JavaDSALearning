41258d4f545a3249e2c484d18bda57363f498845
akshay5568 <adityajangid124@gmail.com>
2026-06-26 14:57:06 +0530

    Today I did not solve any LeetCode problem but I was trying to draw Recursive Tree to understand Recursion.

--- Basics/CountDigit.java
+++ Basics/CountDigit.java
@@ -2,20 +2,24 @@ package Basics;
 
 public class CountDigit {
     public static void main(String[] args) {
-        int nums = 259;
-        int ans= countDigi(nums);
+        int nums = 1342;
+        int ans= RecusriveCountDigi(nums);
         System.out.println(ans);
     }
 
 
     static int countDigi(int num){
-//        String value = String.valueOf(num);
-//        int ans = value.length();
         int counter = 0;
         while (num != 0){
             num = num/10;
             counter++;
         }
         return counter;
+    }
+
+    static int RecusriveCountDigi(int num){
+        if (num <= 0) return 0;
+        int rem = num % 10;
+        return RecusriveCountDigi(num / 10) + rem;
     }
 }

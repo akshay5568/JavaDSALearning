d83ebd38c79067a5581c71f27ab5652701f6e165
akshay5568 <adityajangid124@gmail.com>
2026-06-26 14:57:04 +0530

    Today Solved InterSectProblem LinkedList problem.

---
+++ LinkedList/InterSectProblem.java
@@ -0,0 +1,40 @@
+package LinkedList;
+
+public class InterSectProblem {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        HashSet list = new HashSet();
+//        ListNode temp = headA;
+//
+//        while(temp != null){
+//            list.add(temp);
+//            temp = temp.next;
+//        }
+//
+//        ListNode temp1 = headB;
+//        while(temp1 != null){
+//            if(list.contains(temp1)) return temp1;
+//            temp1 = temp1.next;
+//        }
+//        return null;
+//    }
+}

192a90ce6f0a59673ca96eeaf4804c01c63260ae
akshay5568 <adityajangid124@gmail.com>
2026-06-26 14:57:03 +0530

    Binary Search

---
+++ Basics/Palindrome.java
@@ -0,0 +1,18 @@
+
+
+package Basics;
+
+import java.util.Arrays;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        int arr[]  ={1,2,3,4};
+        for(int i=0; i<arr.length-1; i++){
+            if (i > 0) {
+                arr[i] = arr[i] + arr[i-1];
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}

a445a062215444f813a9c07d40206a982a541085
akshay5568 <adityajangid124@gmail.com>
2026-06-26 14:57:04 +0530

    Today Solved, Linkedlist Leetcode Problems.

---
+++ LinkedList/DeleteNodeFromLinkedlistPersentInArray.java
@@ -0,0 +1,23 @@
+package LinkedList;
+
+public class DeleteNodeFromLinkedlistPersentInArray {
+//    public ListNode modifiedList(int[] nums, ListNode head) {
+//        ListNode temp = head;
+//        ListNode tempHead = new ListNode();
+//        ListNode ans = tempHead;
+//        Set <Integer> list = new HashSet<>();
+//        for(int i : nums){
+//            list.add(i);
+//        }
+//        while(temp != null){
+//            if(list.contains(temp.val)){
+//                temp = temp.next;
+//            }else{
+//                tempHead.next = new ListNode(temp.val);
+//                tempHead = tempHead.next;
+//                temp = temp.next;
+//            }
+//        }
+//        return ans.next;
+//    }
+}
---
+++ Searching/Sqrt.java
@@ -0,0 +1,23 @@
+package Searching;
+
+public class Sqrt {
+    public static void main(String[] args) {
+
+    }
+    static int mySqrt(int x) {
+        if(x < 2) return x;
+        int start = 2;
+        int end = x / 2;
+        while(end >= start){
+            int mid = (start + end) / 2;
+            if((mid * mid) == x){
+                return mid;
+            }else if((long)mid * mid < (long)x){
+                start = mid + 1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return Math.round(end);
+    }
+}

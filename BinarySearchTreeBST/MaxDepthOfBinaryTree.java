35ab5bb446a0e9531980cd8a32391423f3b72718
akshay5568 <adityajangid124@gmail.com>
2026-07-13 18:57:13 +0530

    Today I Solved MaxDepthOfBinaryTree LeetCode Problem.

---
+++ BinarySearchTreeBST/MaxDepthOfBinaryTree.java
@@ -0,0 +1,14 @@
+package BinarySearchTreeBST;
+
+public class MaxDepthOfBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+//     static int maxDepth(TreeNode root) {
+//        if(root == null) return 0;
+//        int maxLeft = maxDepth(root.left);
+//        int maxRight = maxDepth(root.right);
+//        return Math.max(maxLeft,maxRight) + 1;
+//    }
+}

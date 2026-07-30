16f3322f24e21f48a66c96f8b689b0689c06ad5c
akshay5568 <adityajangid124@gmail.com>
2026-07-30 18:17:22 +0530

    Today I implemented a new code version to solve Fibonnaci problem using HashMap LeetCode Problem.

--- Recursion/Fibo.java
+++ Recursion/Fibo.java
@@ -1,9 +1,12 @@
 package Recursion;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.function.Function;
+
 public class Fibo {
     public static void main(String[] args) {
-        int n = 5;
-        int ans = fibo(5);
+        int ans = fibo2(4);
         System.out.println(ans);
     }
 
@@ -12,5 +15,26 @@ public class Fibo {
              return n;
          }
          return fibo(n-1) + fibo(n-2);
+    }
+
+
+    static int fibo2(int n){
+         HashMap<Integer,Integer> hm = new HashMap<>();
+         int ans = fiboCalc(n,hm);
+         System.out.println(ans);
+         return ans;
+    }
+
+    static int fiboCalc(int num,HashMap<Integer,Integer> hm){
+        if (num <= 1) return num;
+        if (hm.containsKey(num)) {
+           return hm.get(num);
+        }
+        int a = fiboCalc(num-1,hm);
+        int b = fiboCalc(num-2,hm);
+        int res = a + b;
+        hm.put(num,res);
+        System.out.println(hm);
+        return res;
     }
 }

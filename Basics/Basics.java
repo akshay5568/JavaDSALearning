913ca0e1e308a9fe33b50d89cc81d1e97ca90617
akshay5568 <adityajangid124@gmail.com>
2026-08-26 17:59:59 +0530

    Today revise Linked list concept agai.

--- Basics/Basics.java
+++ Basics/Basics.java
@@ -1,11 +1,103 @@
 package Basics;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+class Node{
+    int val;
+    Node next = null;
+
+    Node(int val) {
+        this.val = val;
+    }
+
+}
+
 public class Basics {
     public static void main(String[] args) {
+         Node node = new Node(1);
+         Node temp = node;
+
+         node.next = new Node(2);
+         node.next.next = new Node(7);
+         node.next.next.next = new Node(87);
+
+
+         Node loop = temp;
+
+         int valueToBeDelete = 7;
+         Node slow = null;
+         Node fast = temp;
+
+         while(temp != null){
+             if(temp.val == valueToBeDelete){
+                 break;
+             }
+             slow = fast;
+             fast = temp.next;
+             temp = temp.next;
+         }
+
+         if (temp == null) {
+             System.out.println("-1");
+             return;
+         }
+
+         if (slow == null){
+             loop = loop.next;
+         }else slow.next = fast.next;
+
+
+         while(loop != null){
+             System.out.println(loop.val);
+             loop = loop.next;
+         }
+
+
+    }
+
+
+
+    public void reversingLinkedList(Node node){
+
+//        Node node = new Node(1);
+        Node temp = node;
+
+        node.next = new Node(2);
+        node.next.next = new Node(7);
+        node.next.next.next = new Node(87);
+
+
+        Node loop = temp;
+
+
+        System.out.println("Before Reversing!");
+        while(loop != null){
+            System.out.println(loop.val);
+            loop = loop.next;
+        }
+
+        Node prev = null;
+
+
+        System.out.println("After reversing!");
+        while(temp != null){
+            Node temp1 = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = temp1;
+        }
+
+        while(prev != null){
+            System.out.println(prev.val);
+            prev = prev.next;
+        }
 
     }
 }
--- Sorting/Inserstion.java
+++ Sorting/Inserstion.java
@@ -4,20 +4,32 @@ import java.util.Arrays;
 
 public class Inserstion {
     public static void main(String[] args) {
-        int [] arr = {1,2,5,87,3,6};
+        int [] arr = {3,4,1,2};
         inserstion(arr);
         System.out.print(Arrays.toString(arr));
     }
 
+//    static void inserstion(int [] arr){
+//        for (int i = 0; i < arr.length-1; i++) {
+//            for(int j=i+1; j>0; j--){
+//                if (arr[j-1] > arr[j]) {
+//                    int temp = arr[j-1];
+//                    arr[j-1] = arr[j];
+//                    arr[j] = temp;
+//                }else break;
+//            }
+//        }
+//    }
     static void inserstion(int [] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for(int j=i+1; j>0; j--){
-                if (arr[j-1] > arr[j]) {
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }else break;
+        for (int i = 1; i<arr.length; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
+            arr[j + 1] = key;
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
